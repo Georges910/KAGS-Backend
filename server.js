f9447518fd04858876961829345fe1ea5875bf75
Georges910 <georgealsous22@gmail.com>
2021-12-15 17:10:24 +0400

    First commit

---
+++ server.js
@@ -0,0 +1,58 @@
+// Importing express as a function
+const express = require('express');
+
+// Import express-form-data to process HTTP POST requests
+const expressFormData = require('express-form-data');
+
+// Import mongoose to connect to MongoDB Atlas
+const mongoose = require('mongoose');
+const UserModel = require('./models/UserModel.js');
+const userRoutes = require('./routes/user-routes.js');
+
+// Run the express function to get the methods
+const server = express(); 
+
+// Configure express-form-data for server
+// Is for reading HTML form data
+server.use( expressFormData.parse() );
+
+
+// Declare the connnection string
+const connectionString = "mongodb+srv://admin:grg102002@cluster0.fuqx0.mongodb.net/KAGS?retryWrites=true&w=majority"
+
+// Create the mongoose config object
+const connectionConfig = {
+    'useNewUrlParser': true,
+    'useUnifiedTopology': true
+}
+
+// Use the .connect() method to connect to MongoDB
+mongoose
+.connect( connectionString, connectionConfig )
+// If connection successful, then...
+.then(
+    function() {
+        console.log('DB is connected');
+    }
+)
+.catch(
+    function() {
+        console.log('DB not connected');
+    }
+)
+
+// This is only here to quicklt check that the backend is running
+server.get('/', function(req, res){res.send('Welcome')});
+
+
+server.use(
+    '/user', userRoutes         // http://www.something.com/user/
+);
+
+// This is the last thing in your file
+server.listen(
+    3001,
+    function() {
+        console.log('connected to http://localhost:3001/');
+    }
+);
