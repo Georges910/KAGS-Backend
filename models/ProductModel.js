4fce684350fd0d65e9232eb4c4fc6d2383f2719b
Georges910 <93668404+Georges910@users.noreply.github.com>
2021-12-18 09:56:27 +0400

    Add files via upload

---
+++ models/ProductModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+// Create a Schema
+const ProductSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+    }
+)
+
+// Create a Model
+const ProductModel = mongoose.model('products', ProductSchema);
+
+// Export
+module.exports = ProductModel;
